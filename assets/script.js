d1e4c95c88033868785f2f304f88aa98f00e2dcf
The Real Mi <miaolmedillo@gmail.com>
2023-08-09 16:49:57 -0400

    added event listener for save button

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ $(function () {
   //var for id "currentDay" using jQuery
   var currentDayEl = $("#currentDay");
   var currentDayTime = dayjs().format("dddd, MMMM D");
+  var saveBtn = $(".saveBtn");
 
   //Set text to be current day
   currentDayEl.text(currentDayTime);
@@ -26,7 +27,6 @@ $(function () {
     else if (i > currentHour) {
       hour.addClass("future");
     }
-
   }
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -34,13 +34,18 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
+  function saveEvent(event){
+    var clickedButton = $(event.target);
+    var textArea = clickedButton.siblings("textarea");
+    var timeId = clickedButton.parent().attr("id");
+
+    localStorage.setItem(timeId, textArea.val());
+    
+  }
+
+  saveBtn.on("click", saveEvent) //this is the event listener
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
